7810ee7c36605da811667964efc0e7e6685cfedd
Hüseyin Cem <95631429+huseyincembas@users.noreply.github.com>
2022-11-18 15:20:52 +0300

    Add files via upload

---
+++ BirIleYuzArasiAsalSayi/src/BirIleYuzArasiAsalSayi.java
@@ -0,0 +1,17 @@
+public class BirIleYuzArasiAsalSayi {
+    public static void main(String[] args) {
+        int counter = 0;
+        for(int i = 2; i < 100; i++){
+            for(int j = 2; j < i; j++){
+                if(i % j == 0){
+                    counter++;
+                    break;
+                }
+            }
+            if (counter == 0){
+                System.out.print(i + " ");
+            }
+            counter = 0;
+        }
+    }
+}
